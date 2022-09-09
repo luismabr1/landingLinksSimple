b3e9d73b276f89a833399f53b3b69fc76781c2e0
Luis Manuel Brito <lsbrt4@gmail.com>
2022-09-09 09:04:24 -0400

    agrego blank

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <h1 className='grid gap-10 text-modo-negro no-underline mt-20 leading-5 m-0 pb-5 text-6xl text-center dark:text-white'>
+        <h1 className='grid gap-10 text-modo-negro no-underline mt-10
+         leading-5 m-0 pb-5 text-6xl text-center dark:text-white'>
           Bienvenido a <a className= 'hover:text-neutral-200 dark:hover:text-neutral-200 text-rose-500 active:bg-violet-700 ' href="https://modoccs.com" target="_blank" >MoDo </a> Link!
         </h1>
       <main className='min-h-screen, p-16 flex-1 flex flex-col justify-center items-center'>
