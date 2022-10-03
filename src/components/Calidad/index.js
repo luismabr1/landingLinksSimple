0696bc6c373e4773b8f1e2ae3d68ae99ae8a3e50
Luis Manuel Brito <lsbrt4@gmail.com>
2022-10-03 12:23:38 -0400

    act calidad

--- src/components/Calidad/index.js
+++ src/components/Calidad/index.js
@@ -8,10 +8,10 @@ const Calidad = () => {
     return (
             <div className='flex items-center justify-center h-60'>
                         <Link href={link.calidad.urlBackOffice.interno}>
-                            <button className="bg-transparent hover:bg-black text-black dark:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded m-5 p-9">{link.calidad.urlBackOffice.name} Interno</button>
+                            <a className='bg-transparent hover:bg-black text-black dark:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded m-5 p-9' target="_blank" rel="noreferrer">{link.calidad.urlBackOffice.name} Interno</a>
                         </Link>
                         <Link href={link.calidad.urlBackOffice.externo}>
-                            <button className="bg-transparent hover:bg-black text-black dark:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded m-5 p-9">{link.calidad.urlBackOffice.name} Externo</button>
+                            <a className='bg-transparent hover:bg-black text-black dark:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded m-5 p-9' target="_blank" rel="noreferrer">{link.calidad.urlBackOffice.name} Externo</a>
                         </Link>
 {/*                         <Link href={link.calidad.urlBackEnd.interno}>
                             <button className="bg-transparent hover:bg-black text-black dark:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded m-5 p-9">{link.calidad.urlBackEnd.name} Interno</button>
